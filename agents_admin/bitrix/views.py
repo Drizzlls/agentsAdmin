5ce53c034b7fee92baf3730b16e5583b57648090
DrizzllDev <drizzllsvery@yandex.ru>
2023-03-17 12:03:51 +0300

    Добавил вебхук

--- agents_admin/bitrix/views.py
+++ agents_admin/bitrix/views.py
@@ -3,5 +3,5 @@ from bitrix24 import *
 
 
 class DataAPIBitrix24:
-    _WEBHOOK = "https://novoedelo.bitrix24.ru/rest/16/4pzfo9y3vbk2e0t5/"
+    _WEBHOOK = "https://novoedelo.bitrix24.ru/rest/16/l4963lnfla2m21ww/"
     _B = Bitrix24(_WEBHOOK)
